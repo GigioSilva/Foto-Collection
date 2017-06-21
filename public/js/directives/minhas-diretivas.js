f5dafbdefa7c77990d87e92100388f46d29021ee
Giovani Silva <giovani.m.silva@gmail.com>
2017-06-20 23:49:18 -0300

    First Commit

---
+++ public/js/directives/minhas-diretivas.js
@@ -0,0 +1,34 @@
+angular.module('minhasDiretivas', [])
+.directive('meuPainel', function() {
+
+	var ddo = {};
+
+	ddo.restric = "AE";
+
+	ddo.scope = {
+		titulo: '@'
+	};
+
+	ddo.transclude = true;
+
+	ddo.templateUrl = 'js/directives/meu-painel.html';
+
+	return ddo;
+})
+.directive('minhaFoto', function() {
+
+	var ddo = {};
+
+	ddo.restric = "AE";
+
+	ddo.scope = {
+		url: '@',
+		titulo: '@'
+	};
+
+	ddo.transclude = true;
+
+	ddo.templateUrl = 'js/directives/minha-foto.html';
+
+	return ddo;
+});
